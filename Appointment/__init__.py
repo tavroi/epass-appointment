b9c3803efe3ef2a9f7f29f4c8de0fae658487728
Amit Sharma <amit.sharma@verismart.ai>
2024-10-07 16:05:30 +0530

    adding requirements.txt

--- Appointment/__init__.py
+++ Appointment/__init__.py
@@ -81,11 +81,28 @@ async def sub_department_api(request: Request):
                         status_code=HTTP_400_BAD_REQUEST if response.get("is_exception", False) else HTTP_200_OK)
 
 @router.get("/get-time-slots")
-async def time_slots_api(request: Request,department_id=" ",date=int):
+async def time_slots_api(request: Request,officer_id='',department_id=" ",date=int,day=int):
     method_name = inspect.stack()[0][3]
     method, path, ip = get_request_data(request)
     try:
-        response = get_time_slots(department_id,date)
+        response = get_time_slots(officer_id,department_id,date,day)
+    except Exception as e:
+        logger.error(f"Exception Message: {traceback.print_exc()}, Input-Data: , File-Name: {FILE_NAME}, "
+                     f"Method-Name: {method_name}")
+        logger.info(20 * "====")
+        response = {"message": EXCEPTION_MESSAGE, "errorMessage": e.__str__(), "status": False, "is_exception": True}
+    return_response = return_process(data={}, response=response, method=method, path=path, ip=ip,
+                                         member_id=None, method_name=method_name, file_name=FILE_NAME,
+                                         module_name=MODULE_NAME.capitalize(), code=response['code'])
+    return JSONResponse(content=return_response,
+                        status_code=HTTP_400_BAD_REQUEST if response.get("is_exception", False) else HTTP_200_OK)
+
+@router.get("/get-status")
+async def time_slots_api(request: Request):
+    method_name = inspect.stack()[0][3]
+    method, path, ip = get_request_data(request)
+    try:
+        response = get_status()
     except Exception as e:
         logger.error(f"Exception Message: {traceback.print_exc()}, Input-Data: , File-Name: {FILE_NAME}, "
                      f"Method-Name: {method_name}")
--- Appointment/masters.py
+++ Appointment/masters.py
@@ -43,13 +43,46 @@ def get_officials():
         logger.info(20 * "....")
         return {"code": EXCEPTION_CODE, "data": {}, "message": EXCEPTION_MESSAGE, "errorMessage": e.__str__(),
                 "status": False}
+    
+def get_status():
+    try:
+        result=list(db.appointment_status.find({}))
+        return {"data": result, "status": True, "code": READ_CODE, "errorMessage": "", "message": "status list"}
+    except Exception as e:
+        logger.error(f"Exception Message: {traceback.print_exc()}, File-Name: {os.path.basename(__file__)}, "
+                     f"Method-Name: {inspect.stack()[0][3]}")
+        logger.info(20 * "....")
+        return {"code": EXCEPTION_CODE, "data": {}, "message": EXCEPTION_MESSAGE, "errorMessage": e.__str__(),
+                "status": False}
+    
 
-def get_time_slots(department_id, date):
+def get_time_slots(officer_id, department_id, date, day):
     try:
         date_obj = datetime.fromtimestamp(int(date))
 
         now = datetime.now()
         today = now.date()
+
+        # Check if the officer_id exists and get available slot_ids
+        officer_availability = db.officers_availability.find_one({"officer_id": int(officer_id), "day": int(day)})
+        print("officer_availability:",officer_availability)
+
+        if officer_availability:
+            slot_ids = officer_availability.get("slot", [])
+            print(slot_ids)
+            # Fetch the relevant slots for the officer
+            slots = list(db.slots.find({"slot_id": {"$in": slot_ids}}))
+            print("slots:",slots)
+
+            # Immediately return officer's available slots if they exist
+            if slots:
+                available_slots = {slot['slot_time'] for slot in slots}
+                sorted_available_slots = sorted(available_slots, key=lambda x: datetime.strptime(x.split(" - ")[0], '%H:%M'))
+                return {"data": list(sorted_available_slots), "status": True, "code": READ_CODE,
+                        "errorMessage": "", "message": "Available time slots for officer"}
+
+        # If officer_id does not exist or has no slots, proceed with the original flow
+        slots = list(db.slots.find({}))
 
         departments = list(db.department_roster.find({"department_id": int(department_id)}))
 
@@ -59,13 +92,12 @@ def get_time_slots(department_id, date):
 
         available_slots = set()  
 
-        for department in departments: 
-            if date_obj.weekday() in [5, 6]: 
+        for department in departments:
+            if date_obj.weekday() in [6, 7]: 
                 if "saturday" in department.get("week_off", []) or "sunday" in department.get("week_off", []):
                     continue  
 
             dept_start_time = department.get("start_time", 0)  
-            print("dept_start_time:",dept_start_time)
             dept_end_time = department.get("end_time", 86400)  
             lunch_start_time = department.get("lunch_start_time", 46800) 
             lunch_end_time = department.get("lunch_end_time", 48600) 
@@ -75,13 +107,6 @@ def get_time_slots(department_id, date):
             lunch_start = seconds_to_time(lunch_start_time)
             lunch_end = seconds_to_time(lunch_end_time)
 
-            print("dept_start:", dept_start)
-            print("dept_end:", dept_end)
-            print("lunch_start:", lunch_start)
-            print("lunch_end:", lunch_end)
-
-            slots = list(db.slots.find({}))  
-
             for slot in slots:
                 slot_time_str = slot['slot_time']  
                 start_time_str, end_time_str = slot_time_str.split(" - ")
@@ -89,13 +114,17 @@ def get_time_slots(department_id, date):
                 slot_start = datetime.strptime(start_time_str, '%H:%M').time()
                 slot_end = datetime.strptime(end_time_str, '%H:%M').time()
 
+                # Create a datetime object for today's date with the slot start time
+                slot_start_datetime = datetime.combine(now.date(), slot_start)
+
                 if (dept_start <= slot_start < dept_end) and not (lunch_start < slot_start < lunch_end or lunch_start < slot_end <= lunch_end):
                     if date_obj.date() == today:
-                        if slot_start > now:  
+                        if slot_start_datetime > now:  # Compare with datetime object
                             available_slots.add(slot_time_str) 
                     else:
                         available_slots.add(slot_time_str)  
-            sorted_available_slots = sorted(available_slots, key=lambda x: datetime.strptime(x.split(" - ")[0], '%H:%M'))
+
+        sorted_available_slots = sorted(available_slots, key=lambda x: datetime.strptime(x.split(" - ")[0], '%H:%M'))
                        
         return {"data": list(sorted_available_slots), "status": True, "code": READ_CODE, 
                 "errorMessage": "", "message": "Available time slots"}
